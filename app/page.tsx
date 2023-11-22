0dec4ccd8ca78e961778a010e3ee26345b85f2d7
thouel <t.houel@gmail.com>
2023-11-22 17:38:15 +0100

    cicd: trying to understand why in vercel we got a 401 on /api/feed

--- app/page.tsx
+++ app/page.tsx
@@ -53,23 +53,27 @@ const fetchPictures = async (page: number) => {
 }
 
 export default async function Home() {
-  const { isError, message, data } = await fetchPictures(1)
+  const { message } = await fetch(`${getBaseUrl()}/api/test`)
+    .then((res) => res.json())
+    .then((body) => body)
+  log.info(`message rcvd '${message}'`)
+  // const { isError, message, data } = await fetchPictures(1)
 
-  if (isError) {
-    throw new Error(message)
-  }
+  // if (isError) {
+  //   throw new Error(message)
+  // }
 
-  return (
-    <main className='container w-full h-full max-w-5xl min-h-screen px-4 py-8 mx-auto'>
-      <h1 className='mb-4 text-3xl font-bold text-center'>
-        Hello Space & Astronomy
-      </h1>
-      {
-        <div className='grid grid-cols-1 gap-4'>
-          <Pictures pictures={data} />
-          <LoadMore />
-        </div>
-      }
-    </main>
-  )
+  // return (
+  //   <main className='container w-full h-full max-w-5xl min-h-screen px-4 py-8 mx-auto'>
+  //     <h1 className='mb-4 text-3xl font-bold text-center'>
+  //       Hello Space & Astronomy
+  //     </h1>
+  //     {
+  //       <div className='grid grid-cols-1 gap-4'>
+  //         <Pictures pictures={data} />
+  //         <LoadMore />
+  //       </div>
+  //     }
+  //   </main>
+  // )
 }
