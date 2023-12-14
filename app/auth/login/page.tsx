02242b65a35f50f520248bb9e8fa80ac24b00289
thouel <t.houel@gmail.com>
2023-12-14 13:57:33 +0100

    deploy: comment to see where it fails

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -12,35 +12,36 @@ export default async function LoginPage({
   searchParams: { [key: string]: string | string[] | undefined }
 }) {
   log.warn('before session')
-  const session = await getServerSession(options)
+  // const session = await getServerSession(options)
   log.warn('before callbackUrl')
-  const callbackUrl = searchParams['callbackUrl']
+  // const callbackUrl = searchParams['callbackUrl']
 
-  if (session?.user) {
-    if (callbackUrl && typeof callbackUrl === 'string') {
-      redirect(`${callbackUrl}?fromLogin=1`)
-    }
-    redirect('/')
-  }
+  // if (session?.user) {
+  //   if (callbackUrl && typeof callbackUrl === 'string') {
+  //     redirect(`${callbackUrl}?fromLogin=1`)
+  //   }
+  //   redirect('/')
+  // }
   log.warn('before providers')
-  const providers = await fetch(`${getBaseUrl()}/api/auth/providers`)
-    .then((res) => {
-      log.warn('in res', { res })
-      return res.json()
-    })
-    .then((body) => {
-      log.warn('in body', { body })
-      return body
-    })
-  log.warn('providers rcvd', { providers })
+  // const providers = await fetch(`${getBaseUrl()}/api/auth/providers`)
+  //   .then((res) => {
+  //     log.warn('in res', { res })
+  //     return res.json()
+  //   })
+  //   .then((body) => {
+  //     log.warn('in body', { body })
+  //     return body
+  //   })
+  // log.warn('providers rcvd', { providers })
 
   return (
     <>
       <p className='mb-2 text-center'>You choose the way you hop in</p>
       <div className='flex flex-col w-full gap-2 p-2 border border-gray-400 rounded-lg shadow-lg '>
-        <SignInProviders providers={providers} />
+        Providers here
+        {/* <SignInProviders providers={providers} /> */}
       </div>
-      <SignInError />
+      {/* <SignInError /> */}
     </>
   )
 }
